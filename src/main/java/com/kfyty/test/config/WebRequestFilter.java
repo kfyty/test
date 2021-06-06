20e6dbb0633189438059d5e9dccc22c34ecb87e2
kfyty <kfyty725@hotmail.com>
2021-06-06 18:03:51 +0800

    过滤器、拦截器测试

---
+++ src/main/java/com/kfyty/test/config/WebRequestFilter.java
@@ -0,0 +1,30 @@
+package com.kfyty.test.config;
+
+import com.kfyty.support.autoconfig.annotation.Component;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * 描述:
+ *
+ * @author kfyty725
+ * @date 2021/6/6 16:51
+ * @email kfyty725@hotmail.com
+ */
+@Slf4j
+@Component
+@WebFilter
+public class WebRequestFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("do filter: {}", request);
+        chain.doFilter(request, response);
+    }
+}
